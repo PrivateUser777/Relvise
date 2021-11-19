aacd7ee7b12fae65b859444ac17959f3a26501c6
Rustam <atabievrustam@mail.ru>
2021-11-19 20:07:04 +0300

    lazyloading

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,5 @@
 'use strict'
 
-@@include('jquery-3.5.1.min.js')
-
 
 window.addEventListener('DOMContentLoaded', () => {
 	
@@ -43,66 +41,6 @@ for(let anchor of anchors) {
 // // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
 
 
-
-
-//!Слайдер
-// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
-	/*$('.slider').slick({
-		arrows: true,
-		dots: true,
-		adaptiveHeight: true,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		speed: 700,
-		easing: 'linear',
-		infinite: true,
-		initialSlide: 1,
-		autoplay: true,
-		autoplaySpeed: 3000,
-		pauseOnFocus: true,
-		pauseOnHover: true,
-		pauseOnDotsHover: true,
-		draggable: true,
-		swipe: true,
-		touchThreshold: 5,
-		touchMove: true,
-		waitForAnimate: true,
-		centerMode: false,
-		variableWidth: false,
-		rows: 1,
-		slidesPerRow: 1,
-		vertical: false,
-		verticalSwiping: false,
-		fade: false,
-		// asNavFor: "класс слайдера с которым нужно связать";
-		responsive: [
-			{
-					breakpoint: 1000,
-					settings: {
-						slidesToShow: 2
-					}
-			},
-			{
-					breakpoint: 698,
-					settings: {
-						slidesToShow: 1
-					}
-			}
-		],
-		mobileFirst: false,
-	}); */
-// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
-	
-//!Код к спойлерам
-// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
-//  $('.spoilers__title').click(function () {
-// 		if ($('.spoilers').hasClass('one')) {
-// 			$('.spoilers__title').not($(this)).removeClass('active');
-// 			$('.spoilers__text').not($(this).next()).slideUp(300);
-// 		}
-// 		$(this).toggleClass('active').next().slideToggle(300);
-// 	});
-// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
 
 
 });
